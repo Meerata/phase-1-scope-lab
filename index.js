8374c6d9846d04b945d3eef1949501060ed2fab4
Meerata <merata87@gmail.com>
2024-03-25 15:38:24 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// index.js
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  throw new Error("Assignment to constant variable.");
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer,
+};
